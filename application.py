14db805379035e7b53204ed703f819101bdfb877
John Shen <jshen@college.harvard.edu>
2017-07-13 22:09:58 -0400

    Finish auth

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 JSGlue(app)
 
 # global client secret key
-client_secret = ''
+client_secret = 'j5PM7viAPHCZ6DMCIH4NLFvQeCiwVf4H'
 client_id = 'vQ7l7OTmVg8OBET6vwdSXoCDOmvvJ05F'
 
 # ensure responses aren't cached
@@ -23,21 +23,27 @@ if app.config["DEBUG"]:
 
 @app.route("/")
 def index():
-    if not os.environ.get("API_KEY"):
-        raise RuntimeError("API_KEY not set")
-    return render_template("index.html", client_secret=os.environ.get("client_secret"))
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
-    return redirect("https://secure.vendhq.com/connect?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state=None".format(client_id=os.environ.get("CLIENT_ID"),redirect_uri="http://127.0.0.1:5000/login_done"))
+    return redirect("https://secure.vendhq.com/connect?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state=None".format(client_id=client_id,redirect_uri="http://127.0.0.1:5000/login_done"))
 
 @app.route('/login_done')
 def request_token():
-    client_secret=os.environ.get("client_secret")
     code = request.args.get('code','')
+    print(code)
     domain_prefix = request.args.get('domain_prefix','')
     d = {"code":code,"client_id":client_id,"client_secret":client_secret,
-    "grant_type":"authorization_code","redirect_uri":"http://127.0.0.1:5000"}
-    print(d)
+    "grant_type":"authorization_code","redirect_uri":"http://127.0.0.1:5000/login_done"}
     r = requests.post("https://{}.vendhq.com/api/1.0/token".format(domain_prefix),d)
+    global token
+    token = r.json()["access_token"]
+    print(token)
     return "{}".format(r.json())
+@app.route("/inventory")
+def inventory():
+    url = "https://harvardshop.vendhq.com/api/2.0/inventory"
+    response = requests.get(url,headers={"Authorization":"Bearer %s" %token})
+    print(token)
+    return("{}".format(response.text))
