0c6c46236851ec79386be4567999c35a8a44c775
John Shen <jshen@college.harvard.edu>
2017-07-15 14:49:18 -0400

    Display sales

--- application.py
+++ application.py
@@ -3,14 +3,19 @@ import re
 import requests
 from flask import Flask, jsonify, render_template, request, url_for, redirect
 from flask_jsglue import JSGlue
+from .vend import Vend
 
 # configure application
 app = Flask(__name__)
 JSGlue(app)
 
+
 # global client secret key
 client_secret = 'j5PM7viAPHCZ6DMCIH4NLFvQeCiwVf4H'
 client_id = 'vQ7l7OTmVg8OBET6vwdSXoCDOmvvJ05F'
+token = '2tQzNrcZpJ7vDDXgznMJzk_yh2EON82irpEJxKWb'
+
+vend = Vend('harvardshop','2tQzNrcZpJ7vDDXgznMJzk_yh2EON82irpEJxKWb')
 
 # ensure responses aren't cached
 if app.config["DEBUG"]:
@@ -23,27 +28,30 @@ if app.config["DEBUG"]:
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if request.args.get('code',''):
+        code = request.args.get('code','')
+        domain_prefix = request.args.get('domain_prefix','')
+        d = {"code":code,"client_id":client_id,"client_secret":client_secret,
+        "grant_type":"authorization_code","redirect_uri":"http://127.0.0.1:5000"}
+        r = requests.post("https://{}.vendhq.com/api/1.0/token".format(domain_prefix),d)
+        global token
+        token = r.json()["access_token"]
+        print(token)
+    sales = vend.get_sales()
+    del sales['data'][4:]
+    for e in sales['data']:
+        for line in e['line_items']:
+            line['product_name']=vend.get_product(line['product_id'])['data']['name']
+            print(line['product_name'])
+    return render_template("index.html", sales = sales)
 
 @app.route("/login")
 def login():
-    return redirect("https://secure.vendhq.com/connect?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state=None".format(client_id=client_id,redirect_uri="http://127.0.0.1:5000/login_done"))
+    return redirect("https://secure.vendhq.com/connect?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state=None".format(client_id=client_id,redirect_uri="http://127.0.0.1:5000"))
 
-@app.route('/login_done')
-def request_token():
-    code = request.args.get('code','')
-    print(code)
-    domain_prefix = request.args.get('domain_prefix','')
-    d = {"code":code,"client_id":client_id,"client_secret":client_secret,
-    "grant_type":"authorization_code","redirect_uri":"http://127.0.0.1:5000/login_done"}
-    r = requests.post("https://{}.vendhq.com/api/1.0/token".format(domain_prefix),d)
-    global token
-    token = r.json()["access_token"]
-    print(token)
-    return "{}".format(r.json())
 @app.route("/inventory")
 def inventory():
     url = "https://harvardshop.vendhq.com/api/2.0/inventory"
     response = requests.get(url,headers={"Authorization":"Bearer %s" %token})
-    print(token)
-    return("{}".format(response.text))
+    print("{}".format(response.text))
+    return render_template("inventory.html",r=response.json()['data'])
