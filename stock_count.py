f31f63b002ba2ee850ffd341a7b54d59705fc01c
John Shen <jshen@college.harvard.edu>
2017-07-16 19:18:43 -0400

    Finish stock_count.py

--- stock_count.py
+++ stock_count.py
@@ -1,27 +1,44 @@
 import csv
 import requests
+import json
 
 token = '2tQzNrcZpJ7vDDXgznMJzk_Cjr7L8eEIURUw5Lm6'
 MTA = '01f9c6db-e35e-11e2-a415-bc764e10976c'
 GAR = '064dce89-c73d-11e5-ec2a-c92ca32c62a3'
+JFK = '605445f3-3846-11e2-b1f5-4040782fde00'
+BAS = 'f92e438b-3db4-11e2-b1f5-4040782fde00'
+
+#Modify this part
+outlet = MTA
+stock_file = "product-export.csv"
+
+#read products into dict
+with open(stock_file,mode='r',encoding='latin-1') as fp:
+    reader = csv.reader(fp)
+    products = {}
+    for row in reader:
+        try:
+            products[int(row[2])]=row[0]
+        except ValueError:
+            continue
 
 def get_id(sku):
-    payload = {"sku":sku}
-    r = requests.get("https://harvardshop.vendhq.com/api/products",params=payload,headers={"Authorization":"Bearer %s" %token})
     try:
-        return r.json()['products'][0]['id']
+        return products[sku]
     except KeyError:
-        "invalid key (get_id)"
+        print("Key Error for sku {}".format(sku))
 
 with open('stock_count.csv',newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         if len(row) != 2:
+            print("Error: format issue")
             break
         row = [int(e) for e in row]
         sku, count = row
-        payload = {"id":get_id(sku),"inventory":[{"outlet_id":GAR,"count":count}]}
-        print(get_id(sku))
-        print(payload)
+        payload = json.dumps({"id":get_id(sku),"inventory":[{"outlet_id":outlet,"count":count}]})
         r = requests.post("https://harvardshop.vendhq.com/api/products",data=payload,headers={"Authorization":"Bearer %s" %token})
-        print(r.text)
+        if 'status' in r.json():
+            print("Error: {}, sku: {}".format(r.json()['error'],sku))
+        else:
+            print("successfully updated sku {}".format(sku))
