6b3e2a8c334df92048c2b24e2cea16e5e3c154e1
John Shen <jshen@college.harvard.edu>
2017-07-18 22:30:46 -0400

    Finish product-export

--- export_sales.py
+++ export_sales.py
@@ -3,7 +3,7 @@ import requests
 import json
 import sys
 
-token = '2tQzNrcZpJ7vDDXgznMJzk_GaKTP5KLMlyD4v9cX'
+token = '2tQzNrcZpJ7vDDXgznMJzk_Gl2U1U0bXUDRG1KAp'
 outlets = {'MTA':'01f9c6db-e35e-11e2-a415-bc764e10976c',
 'GAR':'064dce89-c73d-11e5-ec2a-c92ca32c62a3',
 'JFK':'605445f3-3846-11e2-b1f5-4040782fde00',
@@ -12,27 +12,38 @@ outlets = {'MTA':'01f9c6db-e35e-11e2-a415-bc764e10976c',
 s = requests.Session()
 s.headers.update({'User-Agent':'theharvardshop_stocktools_JS',"Authorization":"Bearer %s" %token})
 
-r=s.get("https://harvardshop.vendhq.com/api/2.0/products").json()
 products = {}
-for item in r['data']:
-    products[item['id']]=item['variant_name']
-print(products)
+r=s.get("https://harvardshop.vendhq.com/api/2.0/products").json()
+while True:
+    if r['data'] == []:
+        break
+    for item in r['data']:
+        products[item['id']]=item['variant_name']
+    r=s.get("https://harvardshop.vendhq.com/api/2.0/products?after={}".format(r['version']['max'])).json()
+
+def cleanup(product_dict):
+    trash_keys = ['Discount','Checkout Bag Charge (25 cents)','Smart Water 20 OZ.',
+    'Soda/Dasani','Additional Bag Charge ($1)']
+    for key in trash_keys:
+        if key in product_dict:
+            del product_dict[key]
+
 def print_sales(start,end,outlet,csv_file='export_file.csv'):
     sales_dict = {}
     url = "https://harvardshop.vendhq.com/api/2.0/search?date_from={}&date_to={}&order_by=date&order_direction=desc&page_size=1000&status=closed&type=sales&outlet_id={}".format(start,end,outlet)
     r = s.get(url).json()
-    print(url)
     for sale in r['data']:
         for item in sale['line_items']:
-            print(item)
             try:
                 sales_dict[products[item['product_id']]]+=1
             except:
                 try:
                     sales_dict[products[item['product_id']]]=1
                 except:
+                    print('error')
                     continue
-    print(len(r['data']))
+    cleanup(sales_dict)
+    print(len(sales_dict))
     with open(csv_file,'w') as f:
         writer = csv.writer(f)
         writer.writerow(['name','amount sold'])
@@ -41,9 +52,8 @@ def print_sales(start,end,outlet,csv_file='export_file.csv'):
 
 try:
     outlet = sys.argv[1]
-    date = sys.argv[2]
-    csv_file = sys.argv[3]
+    csv_file = sys.argv[2]
 except IndexError:
     print("Format: python3 outlet date")
 
-print_sales('2017-07-17T04:00:00Z','2017-07-18T04:00:00Z',outlets[outlet],csv_file)
+print_sales('2017-07-18T04:00:00Z','2017-07-19T04:00:00Z',outlets[outlet],csv_file)
